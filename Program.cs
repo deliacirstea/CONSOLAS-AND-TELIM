81fb93e4eaeffab5ef3b3d4e8727e057047241bd
deliacirstea <deliutacirstea@gmail.com>
2022-10-18 21:00:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name;
+Console.WriteLine("Bread is ready!");
+Console.WriteLine("Who is the bread for?");
+name = Console.ReadLine();
+Console.WriteLine("It is for " + name + "!");
+Console.WriteLine("Noted " + name + ".");
+
